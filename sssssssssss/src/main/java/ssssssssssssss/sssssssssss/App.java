9004f7fbabddcb96c3b02926827d53d776ff3240
Harshath <harshathg2006@gmail.com>
2025-12-02 23:14:17 +0530

    Initial commit - full project including target

---
+++ sssssssssss/src/main/java/ssssssssssssss/sssssssssss/App.java
@@ -0,0 +1,10 @@
+package ssssssssssssss.sssssssssss;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
